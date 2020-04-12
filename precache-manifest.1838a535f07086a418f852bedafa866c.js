f829b8d20e87a043c7a3a45162d1f5a52717fd5f
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-04-13 00:30:38 +0530

    Updates

---
+++ precache-manifest.1838a535f07086a418f852bedafa866c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12429809a0ef93931c61b3333f9de95",
+    "url": "/advice-app/index.html"
+  },
+  {
+    "revision": "01a92364330bcc6b8442",
+    "url": "/advice-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1145ec0ab29240d9e435",
+    "url": "/advice-app/static/js/2.314519ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice-app/static/js/2.314519ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a92364330bcc6b8442",
+    "url": "/advice-app/static/js/main.f03fd6e8.chunk.js"
+  },
+  {
+    "revision": "b470057d6f9571fcaea2",
+    "url": "/advice-app/static/js/runtime-main.4a36d874.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/advice-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
