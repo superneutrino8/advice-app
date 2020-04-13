6b4dbccd84d3b906d422f4dc276dfeef2b66b94f
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-04-13 11:45:36 +0530

    added axios call to fetch advice from advice slip api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+
+  state = { advice: '' };
+
+  componentDidMount() {
+    this.fetchAdviceData();
+  }
+
+  fetchAdviceData = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        // console.log(response.data.slip.advice);
+        const { advice } = response.data.slip;
+        this.setState({ advice })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
-      <h1>App</h1>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+        </div>
+      </div>
     );
   }
 }
